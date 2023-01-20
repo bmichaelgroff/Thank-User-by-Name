5663663a687822129ffff9a44858641f309a6b9a
bmichaelgroff <bgmichael1400@gmail.com>
2023-01-20 15:55:41 -0700

    fixing code

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const submitBtn = document.getElementById('form-button');
 const userName = document.getElementById('user-name');
 
 submitBtn.addEventListener('click', function() {
-	document.getElementById('user-message').innerHTML = userName;
+	document.getElementById('user-message').innerHTML = userName.value;
 });
